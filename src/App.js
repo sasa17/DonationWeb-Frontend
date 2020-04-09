87c891f3d4d984374649b71ef57528cfc345be1f
lubeina <52536093+lubeina@users.noreply.github.com>
2020-04-09 17:14:58 +0300

    cahnged made in app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { observer } from "mobx-react";
 
-function App() {
+// Components
+import Sidebar from "./Sidebar";
+import SignupForm from "./forms/SignupForm";
+import LoginForm from "./forms/LoginForm";
+import Profile from "./Profile";
+import Dashboard from "./Dashboard";
+
+import authStore from "./stores/authStore";
+
+const App = () => {
+  const getView = () => {
+    return (
+      <Switch>
+        <Redirect exact from="/" to="/login" />
+        <Route path="/signup" component={SignupForm} />
+        <Route path="/login" component={LoginForm} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app" className="container-fluid">
+      <div className="row">
+        <div className="col-2">{authStore.user && <Sidebar />}</div>
+        <div className="content col-10">{getView()}</div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default withRouter(observer(App));
