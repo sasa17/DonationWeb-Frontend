98b00d1e23c8552f1a0d22d1a0c3793fa22efc1e
lubeina <52536093+lubeina@users.noreply.github.com>
2020-04-09 17:15:59 +0300

    login and sign up form created

---
+++ src/forms/SignupForm.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { observer } from "mobx-react";
+
+// Store
+import authStore from "../stores/authStore";
+
+class Signup extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+  };
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    authStore.signup(this.state);
+  };
+
+  render() {
+    const { username, email, password } = this.state;
+
+    if (authStore.user) return <Redirect to="/" />;
+
+    return (
+      <div className="col-6 mx-auto">
+        <div className="card my-5">
+          <div className="card-body">
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="username"
+                  value={username}
+                  name="username"
+                  placeholder="Username"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  value={email}
+                  name="email"
+                  placeholder="Email"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  value={password}
+                  name="password"
+                  placeholder="Password"
+                  onChange={this.handleChange}
+                />
+              </div>
+
+              <button type="submit" className="btn btn-primary">
+                Signup
+              </button>
+              <Link to="/login" className="btn btn-link my-2 my-sm-0">
+                I already have an account
+              </Link>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default observer(Signup);
