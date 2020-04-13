7a341dc9274870ca46fbdbade8830c58ddedf56b
lubeina <52536093+lubeina@users.noreply.github.com>
2020-04-13 19:06:59 +0300

    changes in merge

--- src/Pages/MenuItem.js
+++ src/Pages/MenuItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import menuStore from "./stores/menuStore";
+import menuStore from "../stores/menuStore";
 // import { Link } from "react-router-dom";
 
 class MenuItem extends Component {
@@ -25,58 +25,44 @@ class MenuItem extends Component {
 
     return (
       <div className="card-body">
-        <div className="row">
-          <h5 className="card-item center" style={{ width: "550px" }}>
+        <ul className="list-group">
+          <li className="list-group-item" style={{ color: "darkgreen" }}>
             {this.props.menu.name}
-          </h5>
-        </div>
-        <div className="row" style={{ width: "550px" }}>
-          <button
-            type="button"
-            className="btn btn-success ml-5 mr-2"
-            onClick={handleToggleAdd}
-          >
-            +{" "}
-          </button>
-          <input
-            className="center"
-            style={{ width: "120px" }}
-            type="text"
-            value={this.state.quantity}
-            onChange={(event) =>
-              this.setState({ quantity: Number(event.target.value) })
-            }
-          />
-          <button
-            type="button"
-            className="btn btn-danger mr-5 ml-2"
-            onClick={handleToggleSubtract}
-          >
-            -{" "}
-          </button>
-          <button
-            type="button"
-            className="btn btn-sucess right ml-5"
-            onClick={handleAddItem}
-          >
-            Add Item
-          </button>
-        </div>
+            <button
+              type="button"
+              className="btn btn-success ml-5 mr-2"
+              onClick={handleToggleAdd}
+            >
+              +{" "}
+            </button>
+            <input
+              className="center"
+              style={{ width: "120px" }}
+              type="text"
+              value={this.state.quantity}
+              onChange={(event) =>
+                this.setState({ quantity: Number(event.target.value) })
+              }
+            />
+            <button
+              type="button"
+              className="btn btn-danger mr-5 ml-2"
+              onClick={handleToggleSubtract}
+            >
+              -{" "}
+            </button>
+            <button
+              type="button"
+              className="btn btn-sucess right ml-5"
+              onClick={handleAddItem}
+            >
+              Add Item
+            </button>
+          </li>
+        </ul>
       </div>
     );
   }
 }
-
-const MenuItem = ({ menu }) => {
-  return (
-    <div className="card-body">
-      <ul className="list-group">
-        <li className="list-group-item" style={{ color: "darkgreen" }}>
-          {menu.name} {menu.available_qty}
-        </li>
-      </ul>
-    </div>
-  );
-};
 
 export default MenuItem;
--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -7,7 +7,7 @@ import profileStore from "../stores/profileStore";
 import menuStore from "../stores/menuStore";
 import authStore from "../stores/authStore";
 import MenuItem from "./MenuItem";
-import donationBasketStore from "./stores/donationBasketStore";
+import donationBasketStore from "../stores/donationBasketStore";
 
 class Profile extends Component {
   async componentDidMount() {
@@ -27,14 +27,20 @@ class Profile extends Component {
       donationBasketStore.createDonationBasket();
     };
     return (
-
       <div className="container-fluid position-relative">
         <div class="media container-fluid col-md-6 row-1 position-relative">
-      <img src={require("../FeedForward-wordless.png")} className="w-100 h-100" alt="logo"/>
-      </div>
-        <h2 className="mt-2 center" style={{fontSize: 32, color: "darkgreen", fontWeight: "bold"}}>
-        {profileStore.profile.name} Profile
-          </h2>
+          <img
+            src={require("../FeedForward-wordless.png")}
+            className="w-100 h-100"
+            alt="logo"
+          />
+        </div>
+        <h2
+          className="mt-2 center"
+          style={{ fontSize: 32, color: "darkgreen", fontWeight: "bold" }}
+        >
+          {profileStore.profile.name} Profile
+        </h2>
         <div className="row">
           <div className="col-md-4 col-md-push-8">
             <div className="card width: 18rem;">
@@ -42,40 +48,35 @@ class Profile extends Component {
                 className="img-responsive mx-1 my-5 center"
                 src={profileStore.profile.image}
                 alt={profileStore.profile.name}
-                style={{resizeMode:"contain"}}
+                style={{ resizeMode: "contain" }}
               />
               <div className="card-body">
-              <h5 className="card-title" style={{color: "darkgreen", fontWeight: "bold"}}>
+                <h5
+                  className="card-title"
+                  style={{ color: "darkgreen", fontWeight: "bold" }}
+                >
                   {profileStore.profile.name}
                 </h5>
-                <p className="card-text" style={{color: "darkgreen"}}>
+                <p className="card-text" style={{ color: "darkgreen" }}>
                   {profileStore.profile.description}
                 </p>
-                <p className="card-text" style={{color: "darkgreen"}}>
+                <p className="card-text" style={{ color: "darkgreen" }}>
                   Branch: {profileStore.profile.location}
                 </p>
               </div>
             </div>
-            </div>
-            <div className="col-md-8 col-md-pull-4">
-              <div className="card">
-                {items}
-                <button className="btn-success rounded btn-block" style={{backgroundColor: "darkgreen", color: "snow"}}>
-                  Add Menu Items
-                </button>
-              </div>
+          </div>
+          <div className="col-md-8 col-md-pull-4">
+            <div className="card">
+              {items}
+              <button
+                className="btn-success rounded btn-block"
+                style={{ backgroundColor: "darkgreen", color: "snow" }}
+              >
+                Add Menu Items
+              </button>
             </div>
           </div>
-        </div>
-
-        <div className="card right" style={{ width: "600px" }}>
-          {items}
-          <button
-            className="btn-success rounded btn-block"
-            onClick={handleAddMenuItems}
-          >
-            Add Menu Items
-          </button>
         </div>
       </div>
     );
